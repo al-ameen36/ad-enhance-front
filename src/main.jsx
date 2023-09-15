c8ae49d3d2c5646f0cc96bae3353c45ce4825fd0
al-ameen <muhdabdullahi36@gmail.com>
2023-09-15 13:32:16 +0100

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Posts from './pages/posts/posts.jsx';
+import Post from './pages/posts/post.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Posts />,
+  },
+  {
+    path: "/post",
+    element: <Post />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
